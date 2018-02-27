e974859a5dd951b81bcb261c119761c03f64619b
Daniel Lévy Bureau <daniel.levy@cerema.fr>
2018-02-27 10:32:37 +0100

    COMMIT_DL_BUREAU

--- src/main/java/levy/daniel/application/apptechnic/generationcode/ecriveurs/model/dao/impl/EcriveurDaoInterface.java
+++ src/main/java/levy/daniel/application/apptechnic/generationcode/ecriveurs/model/dao/impl/EcriveurDaoInterface.java
@@ -1,7 +1,11 @@
 package levy.daniel.application.apptechnic.generationcode.ecriveurs.model.dao.impl;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -271,53 +275,462 @@ public class EcriveurDaoInterface
 		this.insererLignesVidesSousLigneDansFichier(
 				pFile, ligneDec, 3, CHARSET_UTF8);
 		
-		/* crée la méthode retrieveByIdMetier(...) */
-		this.creerMethodeRetrieveById(pFile);
+		/* écrit la méthode retrieveByIdMetier(...) */
+		this.ecrireMethodeRetrieveById(pFile);
+		
+		/* écrit la méthode retrieveByAttributs(...) */
+		this.ecrireMethodeRetrieveByAttributs(pFile);
 						
 	} // Fin de ecrireBlocMethodes(...).___________________________________
 	
 
 	
 	/**
-	 * method creerMethodeRetrieveById(
+	 * method ecrireMethodeRetrieveById(
 	 * File pFile) :<br/>
 	 * <ul>
 	 * <li>Crée la Javadoc de la méthode retrieveByIdMetier(...)</li>
 	 * <li>Crée le code de la méthode retrieveByIdMetier(...)</li>
+	 * <li>rajoute 3 lignes vides à la suite.</li>
+	 * <li>Ne fait rien si la méthode a déjà été écrite.</li>
 	 * </ul>
+	 * ne fait rien si pFile est null.<br/>
+	 * ne fait rien si pFile n'existe pas.<br/>
+	 * ne fait rien si pFile n'est pas un fichier simple.<br/>
+	 * <br/>
 	 *
 	 * @param pFile : File.<br/>
 	 * 
 	 * @throws Exception
 	 */
-	private void creerMethodeRetrieveById(
+	private void ecrireMethodeRetrieveById(
 			final File pFile) throws Exception {
 		
+		/* ne fait rien si pFile est null. */
+		if (pFile == null) {
+			return;
+		}
+
+		/* ne fait rien si pFile n'existe pas. */
+		if (!pFile.exists()) {
+			return;
+		}
+
+		/* ne fait rien si pFile n'est pas un fichier simple. */
+		if (!pFile.isFile()) {
+			return;
+		}
+
+		/* Recherche la ligne identifiant. */
+		final String ligneIdentifiant 
+			= "		retrieveByIdMetier";
+	
+		/* Ne fait rien si le code a déjà été écrit. */
+		if (this.existLigneCommencant(
+				pFile, CHARSET_UTF8, ligneIdentifiant)) {
+			return;
+		}
+
 		/* Lecture du template. */
 		final List<String> listeLignes 
 			= this.lireTemplate("dao/interface_dao_retrievebyidmetier");
 		
 		/* substitutions. */
-		final List<String> listSubst1 
+		final List<String> listeSubst1 
 			= this.substituerVariablesDansLigne(
 				listeLignes
 					, VARIABLE_NOM_INTERFACE_METIER
 						, this.nomInterfaceMetier);
+		
+		/* ajoute 3 lignes vides sous la méthode. */
+		this.ajouterLignesVides(3, listeSubst1);
 				
 		/* *************** */
 		/* ENREGISTREMENT. */
-		/* *************** */
-		final String ligneIdentifiant 
-			= "		retrieveByIdMetier";
+		/* *************** */		
+		this.ecrireCode(listeSubst1, pFile);
+
+	} // Fin de ecrireMethodeRetrieveById(...).____________________________
+	
+
+	
+	/**
+	 * method ecrireMethodeRetrieveByAttributs(
+	 * File pFile) :<br/>
+	 * <ul>
+	 * <li>écrit la javadoc de la méthode retrieveByAttributs(...).</li>
+	 * <li>écrit le code de la méthode retrieveByAttributs(...).</li>
+	 * <li>rajoute 3 lignes vides à la suite.</li>
+	 * <li>Ne fait rien si la méthode a déjà été écrite.</li>
+	 * </ul>
+	 * ne fait rien si pFile est null.<br/>
+	 * ne fait rien si pFile n'existe pas.<br/>
+	 * ne fait rien si pFile n'est pas un fichier simple.<br/>
+	 * <br/>
+	 *
+	 * @param pFile : File.<br/>
+	 * 
+	 * @throws Exception
+	 */
+	private void ecrireMethodeRetrieveByAttributs(
+			final File pFile) throws Exception {
 		
+		/* ne fait rien si pFile est null. */
+		if (pFile == null) {
+			return;
+		}
+
+		/* ne fait rien si pFile n'existe pas. */
+		if (!pFile.exists()) {
+			return;
+		}
+
+		/* ne fait rien si pFile n'est pas un fichier simple. */
+		if (!pFile.isFile()) {
+			return;
+		}
+		
+		/* Recherche la ligne identifiant. */
+		final String ligneIdentifiant 
+			= "	 * method retrieveByAttributs";
+	
+		/* Ne fait rien si le code a déjà été écrit. */
 		if (this.existLigneCommencant(
 				pFile, CHARSET_UTF8, ligneIdentifiant)) {
 			return;
 		}
 		
-		this.ecrireCode(listSubst1, pFile);
+		
+		/* écrit la Javadoc. */
+		this.ecrireJavadocMethodeRetrieveByAttributs(pFile);
 
-	} // Fin de creerMethodeRetrieveById(...)._____________________________
+		/* écrit le code. */
+		this.ecrireCodeMethodeRetrieveByAttributs(pFile);
+		
+	} // Fin de ecrireMethodeRetrieveByAttributs(...)._____________________
+	
+
+	
+	/**
+	 * method ecrireJavadocMethodeRetrieveByAttributs(
+	 * File pFile) :<br/>
+	 * <ul>
+	 * <li>écrit la javadoc de la méthode retrieveByAttributs(...).</li>
+	 * </ul>
+	 * ne fait rien si pFile est null.<br/>
+	 * ne fait rien si pFile n'existe pas.<br/>
+	 * ne fait rien si pFile n'est pas un fichier simple.<br/>
+	 * ne fait rien si les attributs du equals ne sont pas définis 
+	 * (this.mapAttributsEquals == null).<br/>
+	 * <br/>
+	 *
+	 *  @param pFile : File.<br/>
+	 *  
+	 * @throws Exception
+	 */
+	private void ecrireJavadocMethodeRetrieveByAttributs(
+			final File pFile) throws Exception {
+		
+		/* ne fait rien si pFile est null. */
+		if (pFile == null) {
+			return;
+		}
+
+		/* ne fait rien si pFile n'existe pas. */
+		if (!pFile.exists()) {
+			return;
+		}
+
+		/* ne fait rien si pFile n'est pas un fichier simple. */
+		if (!pFile.isFile()) {
+			return;
+		}
+		
+		/* ne fait rien si les attributs du equals ne sont pas définis 
+		 * (this.mapAttributsEquals == null). */
+		if (this.mapAttributsEquals == null) {
+			return;
+		}
+
+		final List<String> listeJavadoc = new ArrayList<String>();
+		
+		listeJavadoc.add(DEBUT_JAVADOC_MEMBRE);
+		
+		final String premiereLigne 
+			= DEBUT_LIGNE_JAVADOC_MEMBRE + "method retrieveByAttributs(";
+		
+		listeJavadoc.add(premiereLigne);
+		
+		/* ajoute les paramètres à la méthode dans la Javadoc. */
+		this.ajouterParametresAMethodeJavadoc(listeJavadoc);
+		
+		/* ajoute le corps de la Javadoc. */
+		this.ecrireCorpsJavadoc(listeJavadoc);
+		
+		/* ajoute les paramètres à la Javadoc. */
+		this.ajouterParametresAJavadoc(listeJavadoc);
+		
+		/* ajoute la fin de la Javadoc. */
+		this.ecrireFinJavadoc(listeJavadoc);
+		
+		/* *************** */
+		/* ENREGISTREMENT. */
+		/* *************** */		
+		this.ecrireCode(listeJavadoc, pFile);
+		
+	} // Fin de ecrireJavadocMethodeRetrieveByAttributs(...).______________
+	
+
+	
+	/**
+	 * method ajouterParametresAMethodeJavadoc(
+	 * List&lt;String&gt; pListeJavadoc) :<br/>
+	 * <ul>
+	 * <li>Rajoute les paramètres liés aux attributs 
+	 * de equals() à la méthode en début de Javadoc.</li>
+	 * </ul>
+	 * ne fait rien si les attributs du equals ne sont pas définis 
+	 * (this.mapAttributsEquals == null).<br/>
+	 * <br/>
+	 *
+	 * @param pListeJavadoc : List&lt;String&gt; .<br/>
+	 */
+	private void ajouterParametresAMethodeJavadoc(
+			final List<String> pListeJavadoc) {
+		
+		/* ne fait rien si les attributs du equals ne sont pas définis 
+		 * (this.mapAttributsEquals == null). */
+		if (this.mapAttributsEquals == null) {
+			return;
+		}
+		
+		final int tailleMap = this.mapAttributsEquals.size();
+		int compteur = 0;
+		
+		final Set<Entry<String, String>> entrySet 
+			= this.mapAttributsEquals.entrySet();
+		
+		final Iterator<Entry<String, String>> ite 
+			= entrySet.iterator();
+		
+		while (ite.hasNext()) {
+			
+			compteur++;
+			
+			final Entry<String, String> entry = ite.next();
+			
+			final String nomAttribut = entry.getKey();
+			final String typeAttribut = entry.getValue();
+			
+			String ligneParametre = null;
+			
+			if (compteur == 1) {
+				
+				ligneParametre 
+					= DEBUT_LIGNE_JAVADOC_MEMBRE 
+					+ typeAttribut 
+					+ SEP_ESPACE 
+					+ fournirParametre(nomAttribut);
+				
+				pListeJavadoc.add(ligneParametre);
+				
+			} else if (compteur > 1 && compteur < tailleMap) {
+				
+				ligneParametre 
+				= VIRGULE_JAVADOC_MEMBRE
+				+ typeAttribut 
+				+ SEP_ESPACE 
+				+ fournirParametre(nomAttribut);
+				
+				pListeJavadoc.add(ligneParametre);
+				
+			} else {
+				
+				ligneParametre 
+				= VIRGULE_JAVADOC_MEMBRE
+				+ typeAttribut 
+				+ SEP_ESPACE 
+				+ fournirParametre(nomAttribut) + ") :";
+				
+				pListeJavadoc.add(ligneParametre);
+			}
+			
+		}
+		
+	} // Fin de ajouterParametresAMethodeJavadoc(...)._____________________
+	
+
+		
+	/**
+	 * method ecrireCorpsJavadoc(
+	 * List&lt;String&gt; pListeJavadoc) :<br/>
+	 * <ul>
+	 * <li>écrit le corps de la Javadoc de la 
+	 * méthode retrieveByAttributs(...).</li>
+	 * <li>ajoute le corps dans pListeJavadoc.<br/>
+	 * </ul>
+	 * Ne fait rien si pListeJavadoc == null.<br/>
+	 * <br/>
+	 *
+	 * @param pListeJavadoc : List&lt;String&gt; .<br/>
+	 * 
+	 * @throws Exception
+	 */
+	private void ecrireCorpsJavadoc(
+			final List<String> pListeJavadoc) throws Exception {
+		
+		/* Ne fait rien si pListeJavadoc == null. */
+		if (pListeJavadoc == null) {
+			return;
+		}
+		
+		/* Lecture du template. */
+		final List<String> listeLignes 
+			= this.lireTemplate(
+					"dao/methode_retrievebyattributs_javadoc_interfacedao_corps.txt");
+		
+		/* substitutions. */
+		final List<String> listeSubst1 
+			= this.substituerVariablesDansLigne(
+				listeLignes
+					, VARIABLE_NOM_INTERFACE_METIER
+						, this.nomInterfaceMetier);
+
+		pListeJavadoc.addAll(listeSubst1);
+		
+	} // Fin de ecrireCorpsJavadoc(...).___________________________________
+	
+
+	
+	/**
+	 * method ajouterParametresAJavadoc(
+	 * List&lt;String&gt; pListeJavadoc) :<br/>
+	 * <ul>
+	 * <li>Rajoute les paramètres liés aux attributs 
+	 * de equals() à la Javadoc.</li>
+	 * </ul>
+	 * ne fait rien si les attributs du equals ne sont pas définis 
+	 * (this.mapAttributsEquals == null).<br/>
+	 * <br/>
+	 *
+	 * @param pListeJavadoc : List&lt;String&gt; .<br/>
+	 */
+	private void ajouterParametresAJavadoc(
+			final List<String> pListeJavadoc) {
+		
+		/* ne fait rien si les attributs du equals ne sont pas définis 
+		 * (this.mapAttributsEquals == null). */
+		if (this.mapAttributsEquals == null) {
+			return;
+		}
+		
+		final Set<Entry<String, String>> entrySet 
+			= this.mapAttributsEquals.entrySet();
+		
+		final Iterator<Entry<String, String>> ite 
+			= entrySet.iterator();
+		
+		while (ite.hasNext()) {
+			
+			final Entry<String, String> entry = ite.next();
+			
+			final String nomAttribut = entry.getKey();
+			final String typeAttribut = entry.getValue();
+			
+			final String ligneParametre 
+				= DEBUT_LIGNE_JAVADOC_MEMBRE 
+					+ this.fournirParametre(nomAttribut) 
+					+ SEP_2PTS_AERE 
+					+ typeAttribut 
+					+ SEP_2PTS_AERE + nomAttribut + " du " 
+					+ this.nomInterfaceMetier 
+					+ ".<br/>";
+			
+			pListeJavadoc.add(ligneParametre);
+			
+		}
+		
+	} // Fin de ajouterParametresAJavadoc(...).____________________________
+	
+	
+	
+	/**
+	 * method ecrireFinJavadoc(
+	 * List&lt;String&gt; pListeJavadoc) :<br/>
+	 * <ul>
+	 * <li>écrit la fin de la Javadoc de la 
+	 * méthode retrieveByAttributs(...).</li>
+	 * <li>ajoute la fin dans pListeJavadoc.<br/>
+	 * </ul>
+	 * Ne fait rien si pListeJavadoc == null.<br/>
+	 * <br/>
+	 *
+	 * @param pListeJavadoc : List&lt;String&gt; .<br/>
+	 * 
+	 * @throws Exception
+	 */
+	private void ecrireFinJavadoc(
+			final List<String> pListeJavadoc) throws Exception {
+		
+		/* Ne fait rien si pListeJavadoc == null. */
+		if (pListeJavadoc == null) {
+			return;
+		}
+		
+		/* Lecture du template. */
+		final List<String> listeLignes 
+			= this.lireTemplate(
+					"dao/methode_retrievebyattributs_javadoc_interfacedao_fin.txt");
+		
+		/* substitutions. */
+		final List<String> listeSubst1 
+			= this.substituerVariablesDansLigne(
+				listeLignes
+					, VARIABLE_NOM_INTERFACE_METIER
+						, this.nomInterfaceMetier);
+
+		pListeJavadoc.addAll(listeSubst1);
+		
+	} // Fin de ecrireFinJavadoc(...)._____________________________________
+
+	
+	
+	/**
+	 * method ecrireCodeMethodeRetrieveByAttributs(
+	 * File pFile) :<br/>
+	 * <ul>
+	 * <li>écrit le code de la méthode retrieveByAttributs(...).</li>
+	 * </ul>
+	 * ne fait rien si pFile est null.<br/>
+	 * ne fait rien si pFile n'existe pas.<br/>
+	 * ne fait rien si pFile n'est pas un fichier simple.<br/>
+	 * <br/>
+	 *
+	 *  @param pFile : File.<br/>
+	 *  
+	 * @throws Exception
+	 */
+	private void ecrireCodeMethodeRetrieveByAttributs(
+			final File pFile) throws Exception {
+				
+		/* ne fait rien si pFile est null. */
+		if (pFile == null) {
+			return;
+		}
+
+		/* ne fait rien si pFile n'existe pas. */
+		if (!pFile.exists()) {
+			return;
+		}
+
+		/* ne fait rien si pFile n'est pas un fichier simple. */
+		if (!pFile.isFile()) {
+			return;
+		}
+
+		
+	} // Fin de ecrireCodeMethodeRetrieveByAttributs(...)._________________
+	
 	
 	
 	
