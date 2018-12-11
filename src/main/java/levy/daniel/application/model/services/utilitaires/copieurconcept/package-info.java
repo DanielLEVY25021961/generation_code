e701e347debac69c347a5165d57c73e28cb7c28e
daniel.levy <daniel.levy@cerema.fr>
2018-12-11 17:02:35 +0100

    COMMIT_&_PUSH_DL_BUREAU

---
+++ src/main/java/levy/daniel/application/model/services/utilitaires/copieurconcept/package-info.java
@@ -0,0 +1,31 @@
+/**
+ * CLASSE package-info :<br/>
+ * Package contenant toutes les classes nécessaires 
+ * à la <b>recopie de l'ensemble des packages et classes 
+ * d'un CONCEPT</b> 
+ * contenu dans un projet Eclipse SOURCE vers un projet Eclipse 
+ * CIBLE.<br/>
+ * 
+ * <p>
+ * <span style="text-decoration: underline;">
+ * PRINCIPE DE FONCTIONNEMENT de copieurconcept :
+ * </span>
+ * </p>
+ * <br/>
+ *
+ * - Exemple d'utilisation :<br/>
+ *<br/>
+ * 
+ * - Mots-clé :<br/>
+ * <br/>
+ *
+ * - Dépendances :<br/>
+ * <br/>
+ *
+ *
+ * @author daniel.levy Lévy
+ * @version 1.0
+ * @since 11 déc. 2018
+ *
+ */
+package levy.daniel.application.model.services.utilitaires.copieurconcept;
