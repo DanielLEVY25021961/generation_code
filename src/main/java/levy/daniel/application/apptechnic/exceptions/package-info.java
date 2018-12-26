a0088e2ee736a3e395c2ab8d38084385a8f067c0
daniel.levy <daniel.levy@cerema.fr>
2018-12-26 15:17:43 +0100

    COMMIT_&_PUSH_DL_BUREAU
    après package-info apptechnic/exceptions

--- src/main/java/levy/daniel/application/apptechnic/exceptions/package-info.java
+++ src/main/java/levy/daniel/application/apptechnic/exceptions/package-info.java
@@ -1,6 +1,7 @@
 /**
  * class package-info :<br/>
- * .<br/>
+ * ce package contient toutes les <b>Exceptions typées</b> 
+ * utilisables dans toute l'application.<br/>
  * <br/>
  *
  * - Exemple d'utilisation :<br/>
