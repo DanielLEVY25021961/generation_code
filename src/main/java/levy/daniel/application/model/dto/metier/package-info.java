d12242198706f51db3a13c49fe0455b058c24170
dan <dan@desktop-dan-PC7>
2018-12-28 04:18:20 +0100

    COMMIT_&_PUSH_DL_DOMICILE

--- src/main/java/levy/daniel/application/model/dto/metier/package-info.java
+++ src/main/java/levy/daniel/application/model/dto/metier/package-info.java
@@ -2,8 +2,16 @@
  * CLASSE package-info :<br/>
  * Ce package contient tous les <b>Data Transfer Object (DTO)</b> 
  * relatifs à la COUCHE METIER de l'application.<br/>
- * Les DTO ne servent qu'à <b>véhiculer de l'information</b> 
- * entre les couches VUE, CONTROLLER et MODEL.<br/>
+ * 
+ * <ul>
+ * <li>Les DTO ne servent qu'à véhiculer de l'information 
+ * entre les couches VUE, CONTROLLER et MODEL.</li>
+ * <li>le DTO ne comprend <b>que des attributs typés String</b>.</li>
+ * <li>la VUE alimente le DTO avec les valeurs saisies par l'utilisateur.</li>
+ * <li>le DTO sert ensuite de conteneur de données 
+ * et est envoyé par la VUE vers le CONTROLLER.</li>
+ * </ul>
+ * 
  * <br/>
  *
  * - Exemple d'utilisation :<br/>
