60a2cf1772f66d85e1faa2e6eeb1d3279fea43de
dan <dan@desktop-dan-PC7>
2018-12-30 18:43:26 +0100

    COMMIT_&_PUSH_DL_DOMICILE

--- src/main/java/levy/daniel/application/model/persistence/daoexceptions/technical/impl/DaoFichierInexistantException.java
+++ src/main/java/levy/daniel/application/model/persistence/daoexceptions/technical/impl/DaoFichierInexistantException.java
@@ -9,9 +9,9 @@ import levy.daniel.application.model.persistence.daoexceptions.AbstractDaoExcept
 
 
 /**
- * CLASSE DaoJAXBException :<br/>
- * Exception typée pour les DAO JAXB 
- * pour les erreurs de Marshall, UnMarshall.<br/>
+ * CLASSE DaoFichierInexistantException :<br/>
+ * Exception typée pour les DAO JAXB, JSON, ... 
+ * pour les erreurs de fichier de stockage inexistant.<br/>
  * <br/>
  *
  * - Exemple d'utilisation :<br/>
@@ -29,7 +29,7 @@ import levy.daniel.application.model.persistence.daoexceptions.AbstractDaoExcept
  * @since 18 sept. 2017
  *
  */
-public class DaoJAXBException extends AbstractDaoException {
+public class DaoFichierInexistantException extends AbstractDaoException {
 	
 	// ************************ATTRIBUTS************************************/
 
@@ -45,17 +45,17 @@ public class DaoJAXBException extends AbstractDaoException {
 	 * Logger pour Log4j (utilisant commons-logging).
 	 */
 	private static final Log LOG 
-		= LogFactory.getLog(DaoJAXBException.class);
+		= LogFactory.getLog(DaoFichierInexistantException.class);
 
 	// *************************METHODES************************************/
 	
 
 	/**
 	 * CONSTRUCTEUR D'ARITE NULLE.<br/>
-	 * Permet de construire une DaoJAXBException
+	 * Permet de construire une DaoFichierInexistantException
 	 * sans message ni propagation d'exception cause.<br/>
 	 */
-	public DaoJAXBException() {
+	public DaoFichierInexistantException() {
 		super();
 	} // Fin de CONSTRUCTEUR D'ARITE NULLE.________________________________
 	
@@ -63,13 +63,13 @@ public class DaoJAXBException extends AbstractDaoException {
 
 	/**
 	 * CONSTRUCTEUR D'ARITE 1.<br/>
-	 * Permet de construire une DaoJAXBException
+	 * Permet de construire une DaoFichierInexistantException
 	 * qui propage un message.<br/>
 	 * <br/>
 	 *
 	 * @param pMessage : String : le message à propager.<br/>
 	 */
-	public DaoJAXBException(final String pMessage) {
+	public DaoFichierInexistantException(final String pMessage) {
 		super(pMessage);
 	} // Fin de CONSTRUCTEUR D'ARITE 1.____________________________________
 	
@@ -77,7 +77,7 @@ public class DaoJAXBException extends AbstractDaoException {
 	
 	/**
 	 * CONSTRUCTEUR D'ARITE 1.<br/>
-	 * Permet de construire une DaoJAXBException
+	 * Permet de construire une DaoFichierInexistantException
 	 * qui propage un Throwable qui a causé la présente
 	 * exception.<br/>
 	 * <br/>
@@ -85,7 +85,7 @@ public class DaoJAXBException extends AbstractDaoException {
 	 * @param pCause : Throwable : l'exception qui a
 	 * causé la présente et que l'on veut propager.<br/>
 	 */
-	public DaoJAXBException(final Throwable pCause) {
+	public DaoFichierInexistantException(final Throwable pCause) {
 		super(pCause);
 	} // Fin de CONSTRUCTEUR D'ARITE 1.____________________________________
 	
@@ -93,7 +93,7 @@ public class DaoJAXBException extends AbstractDaoException {
 	
 	/**
 	 * CONSTRUCTEUR D'ARITE 2.<br/>
-	 * Permet de construire une DaoJAXBException
+	 * Permet de construire une DaoFichierInexistantException
 	 * qui propage :<br/>
 	 * - un message,<br/>
 	 * - un Throwable qui a causé la présente
@@ -104,7 +104,7 @@ public class DaoJAXBException extends AbstractDaoException {
 	 * @param pCause : Throwable : l'exception qui a
 	 * causé la présente et que l'on veut propager.<br/>
 	 */
-	public DaoJAXBException(
+	public DaoFichierInexistantException(
 			final String pMessage, final Throwable pCause) {
 		super(pMessage, pCause);
 	} // Fin de CONSTRUCTEUR D'ARITE 2.____________________________________
@@ -113,7 +113,7 @@ public class DaoJAXBException extends AbstractDaoException {
 	
 	/**
 	 * CONSTRUCTEUR D'ARITE 3.<br/>
-	 * Permet de construire une DaoJAXBException
+	 * Permet de construire une DaoFichierInexistantException
 	 * qui propage :<br/>
 	 * - un message,<br/>
 	 * - un Throwable qui a causé la présente
@@ -128,7 +128,7 @@ public class DaoJAXBException extends AbstractDaoException {
 	 * @param pListeExceptions : Liste dans laquelle on veut
 	 * insérer le message de la présente Exception.<br/>
 	 */
-	public DaoJAXBException(
+	public DaoFichierInexistantException(
 			final String pMessage
 				, final Throwable pCause
 					, final List<String> pListeExceptions) {
@@ -139,4 +139,4 @@ public class DaoJAXBException extends AbstractDaoException {
 	
 
 
-} // FIN DE LA CLASSE DaoJAXBException.--------------------------------------
+} // FIN DE LA CLASSE DaoFichierInexistantException.-------------------------
